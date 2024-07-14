92e4daba94ffba02c826a3d85d33e0ced7486473
jagadeeshtholeti <jagadeeshtholeti@gmail.com>
2024-07-14 20:08:12 +0530

    stock price tracker

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import './components/StockTable.css';
+import StockTable from './components/StockTable';
+import Footer from './components/Footer';
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Routes>
+        <Route path='/' element=<StockTable /> />
+        <Route path='/viewstocks' element='..laoding' />
+      </Routes>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
